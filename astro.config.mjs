50f042291c15a57059c01c6dcc61531a92c9a58e
Guido Offermans <guidooffermans@hotmail.com>
2023-03-15 16:53:41 +0100

    optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     preact({
       compat: true,
+    }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
 });
