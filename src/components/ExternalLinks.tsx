3f73beea3436ff69b9d5612cff1ab6bc63f1d1c1
Guido Offermans <guidooffermans@hotmail.com>
2023-03-15 16:18:46 +0100

    add projects and a basic footer

---
+++ src/components/ExternalLinks.tsx
@@ -0,0 +1,12 @@
+export const ExternalLinks = () => {
+  return (
+    <>
+      <a href="https://github.com/GuidoOffermans" target="_">
+        GITHUB
+      </a>
+      <a href="https://www.linkedin.com/in/guido-offermans" target="_">
+        LINKEDIN
+      </a>
+    </>
+  );
+};
